72bff5ae7693a7fa780999ab81fdae141e00fb03
Frank <frankkashere@gmail.com>
2024-08-28 00:05:21 +0300

    Updated Navbar Component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -36,7 +36,7 @@ export default function Header() {
           <div className="flex items-center lg:order-2">
             
             <Link
-              to="#"
+              to="/about"
               className="text-white bg-orange-500 hover:bg-orange-500 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
             >Hire Me</Link>
           </div>
