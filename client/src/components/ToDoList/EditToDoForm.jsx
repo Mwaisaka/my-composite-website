f0ed72dffc9300f8d491a672ec3e4235ce279b34
Frank <frankkashere@gmail.com>
2024-09-02 19:32:47 +0300

    Updated Contact Form

--- client/src/components/ToDoList/EditToDoForm.jsx
+++ client/src/components/ToDoList/EditToDoForm.jsx
@@ -11,14 +11,14 @@ function EditToDoForm({ editToDo, task }) {
     setValue("");
   };
   return (
-    <form onSubmit={handleSubmit} className="ToDo-Form">
+    <form onSubmit={handleSubmit} className="ToDo-Form gap-5">
       <input
         type="text"
         value={value}
         onChange={(e) => setValue(e.target.value)}
-        className="todo-input"
+        className="todo-input-form"
       />
-      <button type="submit" className="todo-btn">
+      <button type="submit" className="todo-btn bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-md shadow-md transition duration-300 ease-in-out">
         Edit Task
       </button>
     </form>
--- client/src/components/ToDoList/ToDoForm.jsx
+++ client/src/components/ToDoList/ToDoForm.jsx
@@ -17,9 +17,9 @@ function ToDoForm ({addToDo}) {
     };
 
     return(
-        <form onSubmit={handleSubmit} className='ToDo-Form'>
+        <form onSubmit={handleSubmit} className='ToDo-Form gap-5'>
             <input type="text" value={value} onChange={(e)=>setValue(e.target.value)} className='todo-input-form' placeholder='What is the task today?'/>
-            <button type="submit" className='todo-btn'>Add Task</button>
+            <button type="submit" className='todo-btn bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-md shadow-md transition duration-300 ease-in-out'>Add Task</button>
         </form>
     )
 };
