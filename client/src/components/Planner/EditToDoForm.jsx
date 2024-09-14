45ce19406375a69b3bd6ea258db9bf9e93466922
Frank <frankkashere@gmail.com>
2024-09-15 00:52:06 +0300

    Updated ToDo Form component

--- client/src/components/Planner/EditToDoForm.jsx
+++ client/src/components/Planner/EditToDoForm.jsx
@@ -12,15 +12,20 @@ function EditToDoForm({ editToDo, task }) {
   };
   return (
     <form onSubmit={handleSubmit} className="ToDo-Form gap-5">
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        className="todo-input-form"
-      />
-      <button type="submit" className="todo-btn bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-md shadow-md transition duration-300 ease-in-out">
-        Edit Task
-      </button>
+      <div className="relative w-full flex items-center rounded-lg">
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className="todo-input-form"
+        />
+        <button
+          type="submit"
+          className="absolute uppercase right-1 top-10 transform -translate-y-1/2 hover:bg-orange-100 text-gray-500 font-bold py-1 px-3 rounded-md transition duration-300 ease-in-out"
+        >
+          Save
+        </button>
+      </div>
     </form>
   );
 }
