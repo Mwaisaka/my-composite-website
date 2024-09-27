b1b8b571de4fe331a6c63cec877f5e02e04d5a81
Frank <frankkashere@gmail.com>
2024-09-27 20:41:54 +0300

    Adding a build folder

--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,7 @@ export default defineConfig({
   root: './client',
   build: {
     outDir: '../dist',
+    emptyOutDir: true,
     chunkSizeWarningLimit: 100000 // Set the chunk size warning limit to 100000 bytes (100 kB)
   }
 })
