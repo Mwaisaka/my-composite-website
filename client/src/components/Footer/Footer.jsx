bc2ecf145536bd1f52c738292771c86985bc63eb
Frank <frankkashere@gmail.com>
2024-09-30 19:52:45 +0300

    Updated footer component

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -50,12 +50,12 @@ export default function Footer() {
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a
-                    href="https://github.com/Mwaisaka"
+                    // href=""
                     className="hover:underline"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    Github
+                    My Blog
                   </a>
                 </li>
                 <li>
