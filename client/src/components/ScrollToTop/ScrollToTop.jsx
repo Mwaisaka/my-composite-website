42886677de61cf79e56893d39c5d5b76e1d030a1
Frank <frankkashere@gmail.com>
2024-10-01 20:46:35 +0300

    Updated contacts page

--- client/src/components/ScrollToTop/ScrollToTop.jsx
+++ client/src/components/ScrollToTop/ScrollToTop.jsx
@@ -14,21 +14,47 @@
 
 // export default ScrollToTop;
 
+// import { useEffect } from "react";
+// import { useLocation } from "react-router-dom";
+
+// function ScrollToTop() {
+//     const location = useLocation();
+
+//     useEffect(() => {
+//         console.log('Pathname changed:', location.pathname);
+//         // Delay scrolling to top by a slight amount to ensure the DOM is fully loaded
+//         setTimeout(() => {
+//             window.scrollTo(0, 0);
+//         }, 0);
+//     }, [location]);
+
+//     return null;
+// }
+
+// export default ScrollToTop;
+
+
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function ScrollToTop() {
-    const location = useLocation();
+  const location = useLocation();
 
-    useEffect(() => {
-        console.log('Pathname changed:', location.pathname);
-        // Delay scrolling to top by a slight amount to ensure the DOM is fully loaded
-        setTimeout(() => {
-            window.scrollTo(0, 0);
-        }, 0);
-    }, [location]);
+  useEffect(() => {
+    // Ensure the scroll happens after the DOM is rendered
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 0);
+  }, [location]);
 
-    return null;
+  // Also ensure that we scroll to top on initial page load (refresh)
+  useEffect(() => {
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 0);
+  }, []);
+
+  return null;
 }
 
 export default ScrollToTop;
